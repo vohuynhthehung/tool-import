6ac4eae53685f11caffa649a9f858fdf83786b4d
Hung Vo <hung.vo@egany.com>
2020-11-02 14:30:28 +0700

    update web app url

--- src/share/constant.js
+++ src/share/constant.js
@@ -1,7 +1,7 @@
-export const APP_URL = "https://nanostcktest.egany.com/api"
+export const APP_URL = "https://api-webapp.nanostick.vn"
 export const LOGIN_URL = `${APP_URL}/auth/login`
-export const APP_ORGID = `${APP_URL}/auth/login?orgid=1000410002`
-export const CAMPAIGNS_URL = "https://nanostcktest.egany.com/api/campaigns"
+export const APP_ORGID = `${APP_URL}/auth/login?orgid=1000345494`
+export const CAMPAIGNS_URL = `${APP_URL}/campaigns`
 
 
 
